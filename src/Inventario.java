2650d8cb5b14f39cf58bac8d81f893f5844b37b9
AntonioCMP <antoniocalderonmatute@gmail.com>
2025-11-23 21:07:55 -0500

    Creacion del CRUD e inventario, y switch case del main junto a funciones

--- src/Inventario.java
+++ src/Inventario.java
@@ -12,4 +12,31 @@ public class Inventario {
 
     //Metodos adicionales de la clase
 
+    public void añadirProducto(Producto producto){
+        this.listaProductos.add(producto);
+        System.out.println("Producto añadido: " + producto.getNombre());
+    }
+
+    public void mostrarProductosDisponibles(){
+        if (this.listaProductos.isEmpty()){
+            System.out.println("Lista vacia");
+        }else{
+        for(Producto producto: listaProductos){
+            System.out.println("Producto: " + producto.getNombre() + "\n" +
+                    "Cantidad disponible: " + producto.getCantidadStock() + "\n" +
+                    "Costo producto: " + producto.getPrecioBase());
+
+        }}
+    }
+
+    public void eliminarProducto(Producto productoEliminado){
+        this.listaProductos.remove(productoEliminado);
+        System.out.println("Producto eliminado: " + productoEliminado.getNombre());
+    }
+
+    public void actualizarInventario(int idProducto, int cantidad){
+        Producto producto = this.listaProductos.get(idProducto);
+        producto.aumentarCantidad(cantidad);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,62 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        Scanner sc = new Scanner(System.in);
         Inventario inventario= new Inventario();
+        int opcion;
+
+        do {
+            mostrarMenu();
+            if (sc.hasNextInt()) {
+                opcion = sc.nextInt();
+                sc.nextLine();
+
+                switch (opcion) {
+                    case 1:
+                        agregarProducto();
+                        break;
+                    case 2:
+                        //resarProfesor();
+                        break;
+                    case 3:
+                        //eliminarAlumno();
+                        break;
+                    case 4:
+                        //eliminarProfesor();
+                        break;
+                    case 5:
+                        //desplegarTodo();
+                        break;
+                    case 6:
+                        System.out.println("\n Saliendo de la aplicacion");
+                        break;
+                    default:
+                        System.out.println("\n Opción no válida. Inténtelo de nuevo.");
+                }
+            } else {
+                System.out.println("\n Entrada no válida. Por favor, ingrese un número.");
+                sc.nextLine();
+                opcion = 0;
+            }
+
+            if (opcion != 6) {
+                System.out.println("\n-------------------------------------------");
+            }
+
+        } while (opcion != 6);
+
+        sc.close();
+    }
+
+
+    public static void mostrarMenu(){}
+
+    public static void agregarProducto(){
+
+
+    }
 
         }
-    }
+
